36c841e9d20ec19256069cf798c79c517d749fab
Yan Smaliak <ysmaliak@gmail.com>
2024-12-08 18:56:18 +0100

    Add `blankSpace`

--- Sources/ShipKitCore/Extensions/String+Extension.swift
+++ Sources/ShipKitCore/Extensions/String+Extension.swift
@@ -3,8 +3,10 @@ import SwiftUI
 
 extension String {
     public static let emptyString: Self = ""
+    public static let blankSpace: Self = " "
 }
 
 extension LocalizedStringKey: @unchecked @retroactive Sendable {
     public static let emptyString: Self = ""
+    public static let blankSpace: Self = " "
 }
