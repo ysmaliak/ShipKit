e5cbfaeddda9785d4ef4a8c646c962a4d2ba3441
Yan Smaliak <ysmaliak@gmail.com>
2024-12-12 11:08:58 +0100

    Fix RevenueCat configuration

--- Sources/ShipKitCore/Configuration.swift
+++ Sources/ShipKitCore/Configuration.swift
@@ -1,5 +1,12 @@
 import Foundation
+import RevenueCat
 
 public struct Configuration {
-    public var revenueCatAPIKey: String?
+    public var revenueCatAPIKey: String? {
+        didSet {
+            if let apiKey = revenueCatAPIKey {
+                Purchases.configure(withAPIKey: apiKey)
+            }
+        }
+    }
 }
