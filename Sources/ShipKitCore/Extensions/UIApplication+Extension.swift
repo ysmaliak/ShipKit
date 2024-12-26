90b3bfb0985cdc29a94551c5d88acf988507d018
Yan Smaliak <ysmaliak@gmail.com>
2024-12-26 20:13:58 +0100

    Rename `keyWindow` to `primaryWindow`

--- Sources/ShipKitCore/Extensions/UIApplication+Extension.swift
+++ Sources/ShipKitCore/Extensions/UIApplication+Extension.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 extension UIApplication {
-    public var keyWindow: UIWindow? {
+    public var primaryWindow: UIWindow? {
         UIApplication.shared.connectedScenes
             .filter { $0.activationState == .foregroundActive }
             .first(where: { $0 is UIWindowScene })
